25af5e20a22aa1d06f8d84a7a00cc10bef51fbc8
Vishrut8898 <vishrut9869@gmail.com>
2021-07-08 19:17:32 +0530

    'changed-folder'

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.delete('/delete/:id', async (req, res) => {
 })
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/build'))
+    app.use(express.static('client/build'))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
